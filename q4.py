a85652a384c0cc9864f9e0ff7e61cc5bb96187e9
Haren-A <haren3171@gmail.com>
2025-07-12 21:34:19 +0530

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,3 +1,27 @@
-import random
+def highest_occurring_char(matrix, m):
+    
+    if not matrix or not matrix[0]:
+        return None, 0
+    
+    input_string = ''.join(matrix[0]).lower()
+    
+    
+    char_count = {}
+    for char in input_string:
+        if char.isalpha():  
+            char_count[char] = char_count.get(char, 0) + 1
+    
+    if not char_count:
+        return None, 0
+    
+    
+    max_char = max(char_count, key=char_count.get)
+    max_count = char_count[max_char]
+    
+    return max_char, max_count
 
-# generate a list of 25 random numberes between 1 and 10 . find the mean, median and mode for these numbers.
+
+A = [['H', 'i', 'p', 'p', 'o'], ['p', 'o', 't', 'a', 'm'], ['u', 's']]
+m = 3
+char, count = highest_occurring_char(A, m)
+print(f"Maximally occurring character is '{char}' & occurrence count is {count}")
